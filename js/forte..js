9053fce1426f11e37c21d38f216c7d08dbef8b30
Kauan Rosendo <kauanfrosendo@gmail.com>
2024-06-07 08:35:23 -0300

    Iniciando o projeto

---
+++ js/forte..js
@@ -0,0 +1,7 @@
+//Aplicação da funcionalidade do Javascript
+document.querySelectorAll('button').forEach(
+   button =>{
+      button.addEventListener('click' , () => {
+        document.body.className = button.id
+      })
+   })
